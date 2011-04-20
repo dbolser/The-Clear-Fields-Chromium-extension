f6e614328793a563b64d51eaeb8c807f69e86a30
Dan Bolser <dan.bolser@gmail.com>
2011-04-21 00:04:46 +0100

    first commit

---
+++ content_script.js
@@ -0,0 +1,46 @@
+clearForms();
+
+function clearForms() {
+  var forms = document.forms;
+  
+  for(i=0; i<forms.length; i++) {
+    clearForm(forms[i]);
+  }
+}
+
+function clearForm(oForm) {
+  var elements = oForm.elements; 
+  
+  oForm.reset();
+
+  for(i=0; i<elements.length; i++) {
+      
+	field_type = elements[i].type.toLowerCase();
+	
+	switch(field_type) {
+	
+		case "text": 
+		case "password": 
+		case "textarea":
+	        case "hidden":	
+			
+			elements[i].value = ""; 
+			break;
+        
+		case "radio":
+		case "checkbox":
+  			if (elements[i].checked) {
+   				elements[i].checked = false; 
+			}
+			break;
+
+		case "select-one":
+		case "select-multi":
+            		elements[i].selectedIndex = -1;
+			break;
+
+		default: 
+			break;
+	}
+    }
+}
